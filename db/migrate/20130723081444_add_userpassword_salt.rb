046e0ca1e20ac242604cf3260644a75b38595a40
afzallakdawala <afzal@adshub.net>
2013-08-01 13:42:39 +0530

    user database migration bug

--- db/migrate/20130723081444_add_userpassword_salt.rb
+++ db/migrate/20130723081444_add_userpassword_salt.rb
@@ -1,7 +1,7 @@
 class AddUserpasswordSalt < ActiveRecord::Migration
   def change
-    add_column :users, :password_hash, :string
-    add_column :users, :password_salt, :string
+    #add_column :users, :password_hash, :string
+    #add_column :users, :password_salt, :string
 
   end
 end
